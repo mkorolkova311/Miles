ad198bb0949880cb6308f7aa8f455940a7fc0860
Margarita Korolkova <mkorolkova311@gmail.com>
2020-11-05 22:43:53 +0300

    добавлен проект Miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float mile = 20;
+        float price = 8456;
+        float bonus = price / mile;
+        System.out.println(bonus);
+    }
+}
